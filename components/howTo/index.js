57d86ae11d562ad15aba0860b24c762516a8e187
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2018-12-18 16:31:02 +0000

    made contribute button in the guide open url in the same tab

--- components/howTo/index.js
+++ components/howTo/index.js
@@ -104,8 +104,6 @@ const HowTo = () => (
         <p className="HowTo__subtitle HowTo__subtitle--is-contribute">Contribute now</p>
           <a
             href="/"
-            target="_blank"
-            rel="noopener noreferrer"
           >
           <Button
             className="HowTo__btn"
